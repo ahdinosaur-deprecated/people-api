304468c6cabd47883165f1fd639f9fe915e20fb7
Michael Williams <dinosaur@riseup.net>
2014-05-14 17:44:28 -0700

    add body-parser

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var jsonld = require('levelgraph-jsonld');
 var app = express();
 var db = jsonld(levelgraph('./db'));
 
+app.use(require('body-parser')());
+
 app.get('/groups', function (req, res, next) {
   // use db.search
   res.json(200, { name: "GET /groups"});
@@ -24,7 +26,7 @@ app.get('/groups/:id', function (req, res, next) {
 
 app.put('/groups/:id', function (req, res, next) {
   var id = req.params.id;
-  var body = req.params.body;
+  var body = req.body;
   // use db.jsonld.put(body, function (err, obj) {})
   res.json(200, { name: "PUT /groups/" + id });
 });
